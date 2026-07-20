06927773a70489db023e24c1f17d1b1cf5f6a274
Webcare-it <ceowebcareit@gmail.com>
2026-07-20 17:56:24 +0600

    Update master.blade.php

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -50,6 +50,9 @@
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
     <!-- Bootstrap-Select CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
 
